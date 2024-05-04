0d29b6e8fa301cdc5cf202f247a9b06a20ef62ed
Dennis Mbithi <denmbithi@gmail.com>
2024-05-05 00:46:34 +0300

    QR code component solution

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      body: ["Outfit", "roboto", "sans-serif"],
+      sans: ["Helvetica", "Arial", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
